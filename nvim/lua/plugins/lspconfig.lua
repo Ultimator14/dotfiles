4e84d062c59321be6915bcedf5acb089be8f5be0
Jan Breig <git@pygos.space>
2025-03-24 22:05:20 +0100

    Nvim: Reformat lspconfig

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -20,18 +20,21 @@ return {
     -- https://microsoft.github.io/language-server-protocol/implementors/servers/
     --]
 
-    local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    local default_capabilities = require('cmp_nvim_lsp').default_capabilities()
     local lspconfig = require('lspconfig')
     local utils = require('utils')
 
-
+    --[
     -- Lsp related keymaps (global)
+    --]
     vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open diagnostics', silent = true })
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev , { desc = 'Go to previous diagnostic', silent = true })
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next , { desc = 'Go to next diagnostic', silent = true })
 
+    --[
     -- Lsp related keymaps (per buffer)
-    local on_attach = function(client, bufnr)
+    --]
+    local default_on_attach = function(client, bufnr)
       vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, { desc = 'Go to declaration', silent = true, buffer = bufnr })
       vim.keymap.set('n', 'gR', vim.lsp.buf.implementation, { desc = 'Show implementations', silent = true, buffer = bufnr })
       vim.keymap.set('n', '<C-q>', vim.lsp.buf.hover, { desc = 'Preview hover information', silent = true, buffer = bufnr })
@@ -45,12 +48,12 @@ return {
     end
 
     local on_attach_texlab = function(client, bufnr)
-      on_attach(client, bufnr)
+      default_on_attach(client, bufnr)
       vim.keymap.set('n', '<localleader>b', ':TexlabBuild<cr>', { desc = 'Build tex file', silent = true })
     end
 
     local on_attach_clangd = function(client, bufnr)
-      on_attach(client, bufnr)
+      default_on_attach(client, bufnr)
 
       local build_file = function()
         local src_name = utils.filename()
@@ -79,161 +82,138 @@ return {
       vim.keymap.set('n', '<localleader>b', build_file, { desc = 'Build c/cpp file', silent = false })
     end
 
-    -- configure lsp servers
-    -- emerge -a dev-python/python-lsp-server
-    -- emerge -a dev-python/python-lsp-ruff
-    -- emerge -a dev-python/pylsp-mypy
-    -- emerge -a dev-python/pylsp-rope  (for rope)
-    -- emerge -a dev-python/pylint-venv  (don't require pylint to be installed in each venv)
-    if vim.fn.executable("pylsp") == 1 then
-      lspconfig.pylsp.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-        --cmd = { "pylsp", "-vvv", "--log-file", "/home/jbreig/nvim_lsp.log"}, -- Debug log , remove in production
-        settings = {
-          pylsp = {
-            plugins = {
-              -- extensive code linting
-              pylint = {
-                enabled = true,
-                args = {"--max-line-length 120", "--init-hook='import pylint_venv; pylint_venv.inithook(quiet=True)'"}
-              },
-              -- very fast linter, replaces flake8 and others
-              ruff = {
-                enabled = true,       -- 3rd party
-                executable = "/usr/bin/ruff", -- Manually set executable to ditch ruff python binding dependencies (which are not installed by gentoo and must be manually installed via pip install)
-                lineLength = 120,
-                -- list of all rules https://beta.ruff.rs/docs/rules/
-                select = {
-                  "F",                        -- pyflakes
-                  "E", "W",                   -- pycodestyle (error, warning)
-                  "C90",                      -- mccabe (complexty)
-                  "I",                        -- isort (import sorting)
-                  "N",                        -- pep8-naming
-                  --"D",                      -- pydocstyle
-                  "UP",                       -- pyupgrade (newer syntax)
-                  "YTT",                      -- sys.version misuse
-                  --"ANN",                    -- missing function annotations
-                  "S",                        -- bandit (security)
-                  "BLE",                      -- blind except
-                  "FBT",                      -- boolean trap
-                  "B",                        -- bugbear
-                  "A",                        -- name shadowing
-                  "COM",                      -- commas
-                  "C4",                       -- unnecessary stuff
-                  "DTZ",                      -- datetime
-                  "DJ",                       -- django
-                  "EM",                       -- errmsg
-                  --"EXE",                    -- shebang
-                  "ISC",                      -- implicit string concat
-                  "ICN",                      -- import conventions
-                  "G",                        -- logging format
-                  "INP",                      -- implicit namespace package
-                  "PIE",                      -- unnecessary stuff
-                  --"T20",                    -- find print statements
-                  "PYI",                      -- typing
-                  "PT",                       -- pytest
-                  "Q",                        -- quotes
-                  "RSE",                      -- unnecessary raise exception stuff
-                  "RET",                      -- unnecessary return stuff
-                  "SLF",                      -- self access
-                  "SIM",                      -- simplify
-                  "TID",                      -- tidy imports
-                  "TCH",                      -- type checking
-                  "INT",                      -- printf
-                  "ARG",                      -- unused arguments
-                  --"PTH",                    -- use pathlib
-                  --"ERA",                    -- commented code
-                  "PD",                       -- pandas code
-                  "PGH",                      -- pygrep hooks
-                  --"PLC", "PLE", "PLR", "PLW"-- pylint
-                  "TRY",                      -- tryceratops (exception anti pattern)
-                  "NPY",                      -- numpy
-                  "RUF"                       -- ruf rules
-                },
-                formatEnabled = true,
-                format = { "I" }              -- autocorrect on reformat
-              },
-              -- completion and renaming
-              rope_completion = {
-                enabled = false        -- disabled by default, 3rd party since v1.12.0
-                                       -- causes pylsp to hang with 100% cpu, disable for now
-              },
-              -- type checker
-              pylsp_mypy = {
-                enabled = true        -- 3rd party
-              },
+    --[
+    -- Additional settings (per lsp)
+    --]
+    local settings_pylsp = {
+      pylsp = {
+        plugins = {
+          -- extensive code linting
+          pylint = {
+            enabled = true,
+            args = {"--max-line-length 120", "--init-hook='import pylint_venv; pylint_venv.inithook(quiet=True)'"}
+          },
+          -- very fast linter, replaces flake8 and others
+          ruff = {
+            enabled = true,       -- 3rd party
+            executable = "/usr/bin/ruff", -- Manually set executable to ditch ruff python binding dependencies (which are not installed by gentoo and must be manually installed via pip install)
+            lineLength = 120,
+            -- list of all rules https://beta.ruff.rs/docs/rules/
+            select = {
+              "F",                        -- pyflakes
+              "E", "W",                   -- pycodestyle (error, warning)
+              "C90",                      -- mccabe (complexty)
+              "I",                        -- isort (import sorting)
+              "N",                        -- pep8-naming
+              --"D",                      -- pydocstyle
+              "UP",                       -- pyupgrade (newer syntax)
+              "YTT",                      -- sys.version misuse
+              --"ANN",                    -- missing function annotations
+              "S",                        -- bandit (security)
+              "BLE",                      -- blind except
+              "FBT",                      -- boolean trap
+              "B",                        -- bugbear
+              "A",                        -- name shadowing
+              "COM",                      -- commas
+              "C4",                       -- unnecessary stuff
+              "DTZ",                      -- datetime
+              "DJ",                       -- django
+              "EM",                       -- errmsg
+              --"EXE",                    -- shebang
+              "ISC",                      -- implicit string concat
+              "ICN",                      -- import conventions
+              "G",                        -- logging format
+              "INP",                      -- implicit namespace package
+              "PIE",                      -- unnecessary stuff
+              --"T20",                    -- find print statements
+              "PYI",                      -- typing
+              "PT",                       -- pytest
+              "Q",                        -- quotes
+              "RSE",                      -- unnecessary raise exception stuff
+              "RET",                      -- unnecessary return stuff
+              "SLF",                      -- self access
+              "SIM",                      -- simplify
+              "TID",                      -- tidy imports
+              "TCH",                      -- type checking
+              "INT",                      -- printf
+              "ARG",                      -- unused arguments
+              --"PTH",                    -- use pathlib
+              --"ERA",                    -- commented code
+              "PD",                       -- pandas code
+              "PGH",                      -- pygrep hooks
+              --"PLC", "PLE", "PLR", "PLW"-- pylint
+              "TRY",                      -- tryceratops (exception anti pattern)
+              "NPY",                      -- numpy
+              "RUF"                       -- ruf rules
+            },
+            formatEnabled = true,
+            format = { "I" }              -- autocorrect on reformat
+          },
+          -- completion and renaming
+          rope_completion = {
+            enabled = false        -- disabled by default, 3rd party since v1.12.0
+                                   -- causes pylsp to hang with 100% cpu, disable for now
+          },
+          -- type checker
+          pylsp_mypy = {
+            enabled = true        -- 3rd party
+          },
 
-              -- error checking, replaced by ruff
-              flake8 = { enabled = false },
-              -- linter for docstring style checking (mostly included in pylint)
-              pydocstyle = { enabled = false },
-              -- linter for style checking (included in flake8/ruff)
-              pycodestyle = { enabled = false },
-              -- linter for complexity (included in flake8/ruff)
-              mccabe = { enabled = false },
-              -- linter for errors (included in flake8/ruff)
-              pyflakes = { enabled = false },
-              -- code formatting (disable in favor of yapf)
-              autopep8 = { enabled = false },
-              -- code formatting (disable in favor of black)
-              yapf = { enabled = false }
-              -- code formatter (replaced by ruff-format)
-              --black = {
-              --  enabled = true,       -- 3rd party
-              --  line_length = 120
-              --},
-            }
-          }
+          -- error checking, replaced by ruff
+          flake8 = { enabled = false },
+          -- linter for docstring style checking (mostly included in pylint)
+          pydocstyle = { enabled = false },
+          -- linter for style checking (included in flake8/ruff)
+          pycodestyle = { enabled = false },
+          -- linter for complexity (included in flake8/ruff)
+          mccabe = { enabled = false },
+          -- linter for errors (included in flake8/ruff)
+          pyflakes = { enabled = false },
+          -- code formatting (disable in favor of yapf)
+          autopep8 = { enabled = false },
+          -- code formatting (disable in favor of black)
+          yapf = { enabled = false }
+          -- code formatter (replaced by ruff-format)
+          --black = {
+          --  enabled = true,       -- 3rd party
+          --  line_length = 120
+          --},
         }
       }
-    end
+    }
 
-    -- emerge -a dev-haskell/haskell-language-server
-    if vim.fn.executable("haskell-language-server-wrapper") == 1 then
-      lspconfig.hls.setup {
-        capabilities = capabilities,
-        on_attach = on_attach
-      }
-    end
-
-    -- sys-devel/clang
-    if vim.fn.executable("clangd") == 1 then
-      lspconfig.clangd.setup{
-        capabilities = capabilities,
-        on_attach = on_attach_clangd
-      }
-    end
-
-    -- dev-tex/texlab
-    if vim.fn.executable("texlab") == 1 then
-      lspconfig.texlab.setup{
-        capabilities = capabilities,
-        on_attach = on_attach_texlab,
-        settings = {
-          texlab = {
-            chktex = {
-              onOpenAndSave = true    -- run chktex on save
-            }
-          }
+    local settings_texlab = {
+      texlab = {
+        chktex = {
+          onOpenAndSave = true    -- run chktex on save
         }
       }
+    }
+
+    -- Use this pylsp config for debugging: {settings = settings_pylsp, cmd = cmd_pylsp_debug}
+    --local cmd_pylsp_debug = { "pylsp", "-vvv", "--log-file", "/home/jbreig/nvim_lsp.log" }  -- Debug log , remove in production
+
+    --[
+    -- Main table containing all the lsp plugins to configure
+    --]
+    local lsp_configs = {
+      {"pylsp",                           lspconfig.pylsp,          {settings = settings_pylsp}},                                 -- dev-python/{python-lsp-{server,ruff},pylsp-{mypy,rope},pylint-venv}
+      {"haskell-language-server-wrapper", lspconfig.hls,            {}},                                                          -- dev-haskell/haskell-language-server
+      {"clangd",                          lspconfig.clangd,         {on_attach = on_attach_clangd}},                              -- sys-devel/clang
+      {"texlab",                          lspconfig.texlab,         {on_attach = on_attach_texlab, settings = settings_texlab}},  -- dev-tex/texlab
+      {"rust-analyzer",                   lspconfig.rust_analyzer,  {}},                                                          -- dev-lang/rust +rust-analyzer
+      {"lua-language-server",             lspconfig.lua_ls,         {}},                                                          -- installed by mason
+      {"ruby-lsp",                        lspconfig.ruby_lsp,       {}}                                                           -- installed by mason
+    }
+
+    -- Configure lsp servers
+    for _, conf in ipairs(lsp_configs) do
+      local executable, language_lsp, config_user = conf[1], conf[2], conf[3]
+      if vim.fn.executable(executable) == 1 then
+        local config_final = vim.tbl_deep_extend('force', {capabilities = default_capabilities, on_attach = default_on_attach}, config_user)
+        language_lsp.setup(config_final)
+      end
     end
 
-    -- lua-language-server (installed by mason)
-    if vim.fn.executable('lua-language-server') == 1 then
-      lspconfig.lua_ls.setup{
-        capabilities = capabilities,
-        on_attach = on_attach
-      }
-    end
-
-    -- ruby language server (installed by mason)
-    if vim.fn.executable('ruby-lsp') == 1 then
-      lspconfig.ruby_lsp.setup{
-        capabilities = capabilities,
-        on_attach = on_attach
-      }
-    end
   end
 }
