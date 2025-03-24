d6e43e7b840b03b56839799d6aa31d85a9a415ad
Jan Breig <git@pygos.space>
2025-03-24 14:57:21 +0100

    Nvim: Add highlighting of current indentation

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -9,5 +9,11 @@ return {
     -- Enable management of surroundings chars
     -- like to nvim-surround but with better keymaps
     require("mini.surround").setup()
+    -- Highlight current indetation scope
+    -- do some changes to improve animation speed
+    require("mini.indentscope").setup({
+      delay = 20,                                -- delay until animation start
+      animation = function(s, n) return 10 end,  -- delay between animation steps
+    })
   end
 }
