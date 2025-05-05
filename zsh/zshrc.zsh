433847e08dcf6dd0e5c8ef1e3efafcd09cf8f4ff
Jan Breig <git@pygos.space>
2025-05-05 22:28:21 +0200

    Zsh: Deduplicate paths

--- zsh/zshrc.zsh
+++ zsh/zshrc.zsh
@@ -1,18 +1,21 @@
+# This directory
+ZSH_HOME="/usr/local/dotfiles/zsh"
+
 # Default config
-source /usr/local/dotfiles/zsh/00-zsh-config.zsh
+source ${ZSH_HOME}/00-zsh-config.zsh
 
 # Package manager and command highlighting
-source /usr/local/dotfiles/zsh/20-zinit.zsh
+source ${ZSH_HOME}/20-zinit.zsh
 
 # Themes
-source /usr/local/dotfiles/zsh/30-theme.zsh
+source ${ZSH_HOME}/30-theme.zsh
 
 # Alias definitions
-source /usr/local/dotfiles/zsh/80-gentoo-alias.zsh
-source /usr/local/dotfiles/zsh/80-misc-alias.zsh
-source /usr/local/dotfiles/zsh/80-fixup-alias.zsh
-source /usr/local/dotfiles/zsh/85-desktop-alias.zsh
-source /usr/local/dotfiles/zsh/85-docker-shell-alias.zsh
+source ${ZSH_HOME}/80-gentoo-alias.zsh
+source ${ZSH_HOME}/80-misc-alias.zsh
+source ${ZSH_HOME}/80-fixup-alias.zsh
+source ${ZSH_HOME}/85-desktop-alias.zsh
+source ${ZSH_HOME}/85-docker-shell-alias.zsh
 
 # Variables export
-source /usr/local/dotfiles/zsh/90-variables.zsh
+source ${ZSH_HOME}/90-variables.zsh
