e663bf43f828dec99c09950bcaabc4b3a01bebf9
Jan Breig <git@pygos.space>
2025-05-05 06:42:25 +0200

    Nvim: Lazyload none-ls

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -1,9 +1,10 @@
 -- Pseudo lsp server to get diagnostics from non-lsp software
 return {
   'nvimtools/none-ls.nvim',
+  event = { "BufReadPre", "BufNewFile", "BufNew" },  -- load on file open, new file and in memory editing
   dependencies = {
-    'nvimtools/none-ls-extras.nvim',     -- Extra sources for none-ls
-    'gbprod/none-ls-php.nvim',           -- Extra php sources for none-ls
+    'nvimtools/none-ls-extras.nvim',                 -- Extra sources for none-ls
+    'gbprod/none-ls-php.nvim',                       -- Extra php sources for none-ls
   },
   config = function()
     local nls = require('null-ls')
