fc31c6661474af16babd6542db197ddddf0f92b8
Jan Breig <git@pygos.space>
2025-12-30 21:32:16 +0100

    Nvim: Fix future fidget deprectation

--- nvim/lua/plugins/fidget.lua
+++ nvim/lua/plugins/fidget.lua
@@ -1,5 +1,14 @@
 -- Show lsp progress in lower right corner
 return {
   'j-hui/fidget.nvim',
-  opts = {}
+  opts = {
+    notification = {
+      window = {
+        avoid = {
+          -- Avoid nvim-tree file explorer
+          "NvimTree"
+        }
+      }
+    }
+  }
 }
