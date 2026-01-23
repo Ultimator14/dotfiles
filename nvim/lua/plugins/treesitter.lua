83d0d9b49a3f43b909f1d86113a8d05a7cfb6f2d
Jan Breig <git@pygos.space>
2026-01-23 19:44:11 +0100

    Nvim: Update nvim-treesitter config to match new default branch

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -2,35 +2,36 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   event = { "BufReadPre", "BufNewFile" },
-  build = ':TSUpdateSync',
-  lazy = false,  -- :TSUpdate should be available at all times
+  build = ':TSUpdate',
+  lazy = false,               -- :TSUpdate should be available at all times
   dependencies = {
     'windwp/nvim-ts-autotag', -- auto close html tags
   },
-  main = "nvim-treesitter.configs",
-  opts = {
-    -- A list of parser names, or "all" (the four listed parsers should always be installed)
+  config = function()
+    -- A list of parser names to install
     -- Do not add 'latex' as it requires node to compile which is not available on all hosts
-    ensure_installed = { "bash", "c", "cpp", "css", "dockerfile", "haskell", "vimdoc", "http", "java", "json", "lua", "make", "markdown", "markdown_inline", "php", "python", "regex", "vim", "yaml" },
-    sync_install = false,   -- synchronous installation
-    auto_install = true,    -- auto install missing
-    --ignore_install = { "javascript" },  -- ignore if ensure_installed is 'all'
+    local parsers = { "bash", "c", "cpp", "css", "dockerfile", "haskell", "vimdoc", "http", "java", "json", "lua", "make",
+      "markdown", "markdown_inline", "php", "python", "regex", "vim", "yaml" }
 
-    highlight = {
-      enable = true,
+    require("nvim-treesitter").install(parsers)
 
-      --disable for large files
-      disable = function(lang, buf)
-        local max_filesize = 1024 * 1024     -- 1 MB
-        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+    -- Auto enalbe for all known filetypes
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = parsers,
+      callback = function()
+        vim.treesitter.start()
+      end
+    })
+
+    --disable highlighting for large files
+    vim.api.nvim_create_autocmd("BufReadPre", {
+      callback = function(args)
+        local max_filesize = 1024 * 1024   -- 1 MB
+        local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(args.buf))
         if ok and stats and stats.size > max_filesize then
-          return true
+          vim.treesitter.stop(args.buf)
         end
-      end,
-      -- alternatively: disable per parser
-      --disable = { "c", "rust" },
-
-      additional_vim_regex_highlighting = false,
-    },
-  }
+      end
+    })
+  end
 }
